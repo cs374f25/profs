c966a59b8a1b45a8496f5ee26c91a522c65923af
Chris Mayfield <mayfiecs@jmu.edu>
2025-11-04 20:51:05 -0500

    Add index view for the front page

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,17 @@
 """Initialize the Flask application, database session, and appbuilder object."""
 
 from flask import Flask
-from flask_appbuilder import AppBuilder
+from flask_appbuilder import AppBuilder, IndexView
 from flask_appbuilder.utils.legacy import get_sqla_class
+
+class MyIndexView(IndexView):
+    index_template = 'index.jinja'
 
 app = Flask(__name__)
 app.config.from_object("config")
 
 db = get_sqla_class()()
-appbuilder = AppBuilder()
+appbuilder = AppBuilder(indexview=MyIndexView)
 
 with app.app_context():
     db.init_app(app)
