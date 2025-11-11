5b54ce0267e41aebc8bf3090bd50430db8b073a5
Chris Mayfield <mayfiecs@jmu.edu>
2025-11-10 19:52:06 -0500

    Add sequences to the drop_ab.sql script

--- archive/drop_ab.sql
+++ archive/drop_ab.sql
@@ -1,11 +1,16 @@
--- Drop all tables starting with 'ab_'
-
+-- Drop all tables and sequences starting with 'ab_'
 
 SELECT 'DROP TABLE IF EXISTS ' || tablename || ' CASCADE;'
 FROM pg_tables
 WHERE schemaname = 'public'
-  AND tablename LIKE 'ab_%';
+  AND tablename LIKE 'ab_%'
+ORDER BY tablename;
 
+SELECT 'DROP SEQUENCE IF EXISTS ' || sequencename || ' CASCADE;'
+FROM pg_sequences
+WHERE schemaname = 'public'
+  AND sequencename LIKE 'ab_%'
+ORDER BY sequencename;
 
 DROP TABLE IF EXISTS ab_group CASCADE;
 DROP TABLE IF EXISTS ab_group_role CASCADE;
@@ -18,3 +23,15 @@ DROP TABLE IF EXISTS ab_user CASCADE;
 DROP TABLE IF EXISTS ab_user_group CASCADE;
 DROP TABLE IF EXISTS ab_user_role CASCADE;
 DROP TABLE IF EXISTS ab_view_menu CASCADE;
+
+DROP SEQUENCE IF EXISTS ab_group_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_group_role_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_permission_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_permission_view_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_permission_view_role_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_register_user_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_role_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_user_group_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_user_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_user_role_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS ab_view_menu_id_seq CASCADE;
